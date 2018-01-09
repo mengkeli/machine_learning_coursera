6d55f278c0ad8e310aecfba1dd0a08790e9d13ab
mengke <15652509013@163.com>
2018-01-09 16:23:11 +0800

    add

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,9 +22,21 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_list = C_list;
+pre_err = zeros(size(C_list, 2), size(sigma_list, 2));
+for i = 1 : length(C_list)
+    for j = 1 : length(sigma_list)
+        model = svmTrain(X, y, C_list(i), @(x1, x2) gaussianKernel(x1, x2, sigma_list(j)));
+        predictions = svmPredict(model, Xval);
+        pre_err(i, j) = mean(double(predictions ~= yval));
+    end
+end
 
-
-
+[min_vec , min_id1] = min(pre_err);
+[min, min_id2] = min(min_vec);
+C = C_list(min_id1(min_id2));
+sigma = sigma_list(min_id2);
 
 
 
